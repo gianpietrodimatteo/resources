6b9b1a7e379b4774438beab8caf441ea1898ea4e
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2019-12-19 18:05:51 -0300

    altered to st launching algorythm

--- launch-st-terminal-example.sh
+++ launch-st-terminal-example.sh
@@ -5,3 +5,8 @@ st -n project -e sh -c 'mvn -f $PPROJSDIR/prompt-key/ spring-boot:run;' &
 st -n project -e sh -c 'cd $PPROJSDIR/prompt-server/; ./mvnw' &
 st -n project -e sh -c 'yarn --cwd $PPROJSDIR/prompt-client/ install; yarn --cwd $PPROJSDIR/prompt-client/ start'
 
+# Or, piping to less:
+st -n project -e sh -c 'mvn -f $PPROJSDIR/prompt-key/ spring-boot:run | less +F' &
+st -n project -e sh -c 'cd $PPROJSDIR/prompt-server/; ./mvnw | less +F' &
+st -n project -e sh -c 'yarn --cwd $PPROJSDIR/prompt-client/ install; yarn --cwd $PPROJSDIR/prompt-client/ start | less +F' &
+
